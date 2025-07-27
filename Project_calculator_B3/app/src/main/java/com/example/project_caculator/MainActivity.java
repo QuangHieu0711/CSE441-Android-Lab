484738c4eb5f34e05c1db64a42433d08057e2537
QuangHieu0711 <nguyenquanghieugl2019@gmail.com>
2025-07-27 15:23:42 +0700

    Add + and - to calculator B3

---
+++ Project_calculator_B3/app/src/main/java/com/example/project_caculator/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.project_caculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    EditText edt1, edt2, edt3;
+    Button btncong, btntru;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Ánh xạ view
+        edt1 = findViewById(R.id.edta);
+        edt2 = findViewById(R.id.edtb);
+        edt3 = findViewById(R.id.edtc);
+
+        btncong = findViewById(R.id.btncong);
+        btntru = findViewById(R.id.btntru);
+
+        // Xử lý nút CỘNG
+        btncong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int a = Integer.parseInt(edt1.getText().toString());
+                    int b = Integer.parseInt(edt2.getText().toString());
+                    int tong = a + b;
+                    edt3.setText(a + " + " + b + " = " + tong);
+                } catch (NumberFormatException e) {
+                    edt3.setText("Vui lòng nhập số hợp lệ");
+                }
+            }
+        });
+
+        // Xử lý nút TRỪ
+        btntru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int a = Integer.parseInt(edt1.getText().toString());
+                    int b = Integer.parseInt(edt2.getText().toString());
+                    int hieu = a - b;
+                    edt3.setText(a + " - " + b + " = " + hieu);
+
+                } catch (NumberFormatException e) {
+                    edt3.setText("Vui lòng nhập số hợp lệ");
+                }
+            }
+        });
+    }
+}
