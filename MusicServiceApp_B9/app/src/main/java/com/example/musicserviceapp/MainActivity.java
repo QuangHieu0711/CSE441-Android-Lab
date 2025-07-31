c200431fe308be2420e0cb14e1d1ea639b7ce410
QuangHieu0711 <nguyenquanghieugl2019@gmail.com>
2025-07-31 20:50:33 +0700

    B9 Music Service App

---
+++ MusicServiceApp_B9/app/src/main/java/com/example/musicserviceapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.musicserviceapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton btnPlay, btnStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPlay = findViewById(R.id.btnplay);
+        btnStop = findViewById(R.id.btnstop);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                startService(intent); // Bắt đầu phát nhạc
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                stopService(intent); // Dừng nhạc
+            }
+        });
+    }
+}
